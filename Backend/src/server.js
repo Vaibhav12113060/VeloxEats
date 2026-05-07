d31359a42cee3bdf0a69404940f265f9dff33dd6
Vaibhav12113060 <vaibhavkum4@gmail.com>
2026-05-08 01:16:16 +0530

    feat: Integrated APIs routing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,30 +6,37 @@ const dotenv = require("dotenv");
 
 const connectDB = require("./Config/db.js");
 
-// Configuration
+// Routes (DIRECT IMPORT)
+const authRoutes = require("./Routes/authRoutes.js");
+const addressRoutes = require("./Routes/addressRoutes.js");
+const restaurantRoutes = require("./Routes/restaurantRoutes.js");
+const menuCategoryRoutes = require("./Routes/menuCategoryRoutes.js");
+const menuItemRoutes = require("./Routes/menuItemRoutes.js");
+const cartRoutes = require("./Routes/cartRoutes.js");
+const orderRoutes = require("./Routes/orderRoutes.js");
+const orderItemRoutes = require("./Routes/orderItemRoutes.js");
+const paymentRoutes = require("./Routes/paymentRoutes.js");
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
+// CORS
 const allowedOrigins = [
-  "http://localhost:5173", // Local Vite
+  "http://localhost:5173",
   "http://192.168.0.106:5173",
   process.env.FRONTEND_URL,
 ];
 
-// Middlewares
 app.use(
   cors({
     origin: (origin, callback) => {
-      // allow requests with no origin (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
 
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        // Agar origin whitelist mein hai, toh allow
+      if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        // Agar nahi hai, toh block
         callback(new Error("Not allowed by CORS"));
       }
     },
@@ -37,22 +44,29 @@ app.use(
     credentials: true,
   }),
 );
+
 app.use(express.json());
 app.use(morgan("dev"));
 
-// API Routes
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/address", addressRoutes);
+app.use("/api/v1/restaurants", restaurantRoutes);
+app.use("/api/v1/menu-categories", menuCategoryRoutes);
+app.use("/api/v1/menu-items", menuItemRoutes);
+app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/order-items", orderItemRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
-// Welcome/Root Route
+// Root route
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send(
-      "<h1>Welcome To VeloxEats: The Real-time, Hyperlocal food delivery platform !!!!!!!</h1>",
-    );
+  res.send("VeloxEats API Running...");
 });
 
-// Port & Listen
+// PORT
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`.bgCyan);
+  console.log(`Server running on PORT: ${PORT}`.bgCyan);
 });
